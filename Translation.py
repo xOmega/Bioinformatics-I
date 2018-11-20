4546d68b688ce7be8e8c2bd11ea6fa186d9158ae
Neeraj kulkarni <neerajkulkarni@Neerajs-MacBook-Air.local>
2018-11-19 17:32:25 -0800

    Added Protein Translation Code using Genetic code

---
+++ Translation.py
@@ -0,0 +1,37 @@
+gencode = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
+
+
+def translate(file_name):
+    with open(file_name, "r") as f:
+        seq = f.read()
+    if len(seq) % 3 != 0:
+        print "Incorrect format: Incomplete codon at the end"
+        return None, 0
+    protein_seq = ""
+    for i in range(0, len(seq), 3):
+        protein_seq += gencode[seq[i:i+3]]
+
+    return protein_seq, 1
+
+
+prot_seq, status = translate("nuc1.txt")
+if status:
+    with open("protein.txt", "w") as f:
+        f.write(prot_seq)
+
