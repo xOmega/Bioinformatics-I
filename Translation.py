3cf84f239c50208e9d6dc345ec59e0b53eb00d72
Neeraj Kulkarni <nee.rajkul@yahoo.co.in>
2019-02-09 12:45:08 -0800

    Modified Translation
    Corrected START and STOP codon mechanism in the translation process

--- Translation.py
+++ Translation.py
@@ -1,37 +1,46 @@
-gencode = {
-    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
-    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
-    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
-    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
-    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
-    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
-    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
-    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
-    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
-    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
-    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
-    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
-    'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
-
+Gencode = {
+    'AUA': 'I', 'AUC': 'I', 'AUU': 'I', 'AUG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACU': 'T',
+    'AAC': 'N', 'AAU': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGU': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CUA': 'L', 'CUC': 'L', 'CUG': 'L', 'CUU': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCU': 'P',
+    'CAC': 'H', 'CAU': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGU': 'R',
+    'GUA': 'V', 'GUC': 'V', 'GUG': 'V', 'GUU': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCU': 'A',
+    'GAC': 'D', 'GAU': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGU': 'G',
+    'UCA': 'S', 'UCC': 'S', 'UCG': 'S', 'UCU': 'S',
+    'UUC': 'F', 'UUU': 'F', 'UUA': 'L', 'UUG': 'L',
+    'UAC': 'Y', 'UAU': 'Y', 'UAA': 'STOP', 'UAG': 'STOP',
+    'UGC': 'C', 'UGU': 'C', 'UGA': 'STOP', 'UGG': 'W'
+}
 
 def translate(file_name):
     with open(file_name, "r") as f:
         seq = f.read()
-    if len(seq) % 3 != 0:
-        print "Incorrect format: Incomplete codon at the end"
-        return None, 0
-    protein_seq = ""
-    for i in range(0, len(seq), 3):
-        protein_seq += gencode[seq[i:i+3]]
+    amino_seq = ""
+    flag = False
+    i = 0
+    while i <= len(seq) - 3:
+        codon = seq[i: i + 3]
+        if not flag and codon == "AUG":
+            flag = True
+        if flag:
+            amino_seq += Gencode[codon]
+            if codon in ('UAA', 'UGA', 'UAG'):
+                break
+            else:
+                amino_seq += "-"
+            i += 3
+        else:
+            i += 1
+    return amino_seq
 
-    return protein_seq, 1
 
-
-prot_seq, status = translate("nuc1.txt")
-if status:
+if __name__ == '__main__':
+    prot_seq= translate("nuc1.txt")
     with open("protein.txt", "w") as f:
         f.write(prot_seq)
 
